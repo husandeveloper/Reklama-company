530f73bff107fe6a6ca3567a2efda355393fd917
husandeveloper <nevertolibov13@gmail.com>
2021-08-07 11:52:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function tepaga1(){
+    let chiqar = document.querySelector('.tayoq1');
+    chiqar.classList.toggle('tepaga1');
+}
+
+function tepaga2(){
+    let chiqar = document.querySelector('.tayoq2');
+    chiqar.classList.toggle('tepaga2');
+}
+
+
+function tepaga3(){
+    let chiqar = document.querySelector('.tayoq3');
+    chiqar.classList.toggle('tepaga3');
+}
+
+
+function tepaga4(){
+    let chiqar = document.querySelector('.tayoq4');
+    chiqar.classList.toggle('tepaga4');
+}
+
+
